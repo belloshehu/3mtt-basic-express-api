b4c464e0cb2f0233acb5749bfefc2b075da81018
Bello Shehu <belloshehu1@gmail.com>
2025-06-20 00:53:32 +0100

    Add base route handle

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = 3000;
 app.use(cors());
 app.use(express.json());
-app.use("/api/users", userRouter);
+app.use("/api/items", userRouter);
 
 app.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}`);
